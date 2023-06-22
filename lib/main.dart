4652d242213f8e35761963d194e6e87ce2e364ea
davidrockkj <davidrockkj@gmail.com>
2023-06-22 17:27:46 -0300

    Column and Row

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,45 @@ class MeuApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child: Container(
-            margin: const EdgeInsets.only(top: 50.0),
-            height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            child: const Text('Olá guys'),
+          child: Column(
+            // verticalDirection: VerticalDirection.up,
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            // mainAxisAlignment: MainAxisAlignment.end,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: const Text('Container 1'),
+              ),
+              const SizedBox(
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+                child: const Text('Container 2'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.lightGreen,
+                child: const Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+                height: 100.0,
+                color: Colors.indigo[700],
+                child: const Text('Container 4'),
+              ),
+            ],
           ),
         ),
       ),
