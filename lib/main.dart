38b24984e136da09015cd3a9e5d8bb3611fc3871
davidrockkj <davidrockkj@gmail.com>
2023-07-03 17:12:02 -0300

    Finalização do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,11 @@ class MeuApp extends StatelessWidget {
         backgroundColor: Colors.indigo,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
             children: [
-              const SizedBox(height: 100.0),
+              // const SizedBox(height: 100.0), - usada para "centralizar" o conteúdo
+
               CircleAvatar(
                 backgroundImage: const AssetImage('imagens/davatar.png'),
                 backgroundColor: Colors.indigo.shade300,
@@ -45,37 +48,42 @@ class MeuApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
-              // TELEFONE --------------------------
               Container(
-                height: 60.0,
-                margin: const EdgeInsets.fromLTRB(60.0, 20.0, 60.0, 0.0),
+                height: 40,
+                width: 245,
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.indigo[800],
+                  borderRadius: BorderRadius.circular(100),
                 ),
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 10.0
+                child: const Divider(
+                  height: 20,
+                  // thickness: 5,
+                  color: Colors.white,
+                ),
+              ),
+              // TELEFONE --------------------------
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100),
+                  //set border radius more than 50% of height and width to make circle
+                ),
+                margin: const EdgeInsets.fromLTRB(60.0, 0.0, 60.0, 5.0),
+                color: Colors.indigo[800],
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                        size: 25.0,
                       ),
-                    ),
-                    Icon(
-                      Icons.phone,
-                      color: Colors.white,
-                      size: 25.0,
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Text(
-                      '+55 99 99999-9999',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Source Sans',
-                          fontSize: 20.0),
-                    )
-                  ],
+                      title: Text(
+                        '+55 99 99999-9999',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Source Sans',
+                            fontSize: 20.0),
+                      )),
                 ),
               ),
               // EMAIL --------------------------
@@ -84,26 +92,24 @@ class MeuApp extends StatelessWidget {
                   borderRadius: BorderRadius.circular(100),
                   //set border radius more than 50% of height and width to make circle
                 ),
+                margin: const EdgeInsets.fromLTRB(60.0, 10.0, 60.0, 20.0),
                 color: Colors.indigo[800],
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 60.0,
-                ),
-                child: const ListTile(
-                  leading: 
-                    Icon(
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: ListTile(
+                    leading: Icon(
                       Icons.mail_rounded,
                       color: Colors.white,
                       size: 25.0,
                     ),
-                  title:
-                    Text(
+                    title: Text(
                       'email@gmail.com',
                       style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Source Sans',
-                      fontSize: 20.0),
+                          color: Colors.white,
+                          fontFamily: 'Source Sans',
+                          fontSize: 20.0),
                     ),
+                  ),
                 ),
               ),
             ],
